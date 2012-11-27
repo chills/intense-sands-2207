1201a96530957cbe40c205eaf87fb6b27f18c4da
chills <cathhills@gmail.com>
2012-11-27 14:05:43 +1100

    New app
    Signed-off-by: chills <cathhills@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IntenseSands2207::Application.config.secret_token = 'cba134860044357654da0b345261936ec9d72a6c796edc1ae03849ebb576654e925e2eae8974afe2fef99db4165a562a9a8c45fa196243a62105db2f4cd6e631'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+IntenseSands2207::Application.config.session_store :cookie_store, key: '_intense-sands-2207_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# IntenseSands2207::Application.config.session_store :active_record_store
