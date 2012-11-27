fd4c7ee717de8428f6728623d2ba49a3041967a0
chills <cathhills@gmail.com>
2012-11-27 16:55:42 +1100

    map root action home controller
    Signed-off-by: chills <cathhills@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 IntenseSands2207::Application.routes.draw do
 
-  get "home/index"
+  root :to => "home#index"
+  
+  #get "home/index"
 
   # match "/index" => "pages#index"
   # The priority is based upon order of creation:
